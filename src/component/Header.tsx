38f738498bb4cbe40988074ed4904bfdc959e473
NikolasTsm <ntsamitr@ece.auth.gr>
2025-08-20 00:30:27 +0300

    Improve header UI

--- src/component/Header.tsx
+++ src/component/Header.tsx
@@ -1,40 +1,67 @@
-import React from 'react';
-import './Header.css';
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import React, { useEffect, useState } from "react";
+import "./Header.css";
 import { FaUserTie, FaUserCircle, FaEnvelope } from "react-icons/fa";
-import { Link } from 'react-router-dom';
-import logo from '.././/assets/Logo.png';
+import { Link } from "react-router-dom";
+import logo from ".././/assets/Logo.png";
+import axios from "axios";
+import { API_BASE_URL } from "../config/Config";
+import type { User } from "../interface/User";
 
-interface HeaderProps{
-    shownav?: boolean;
+interface HeaderProps {
+  shownav?: boolean;
 }
 
-export default function Header({shownav= true}: HeaderProps) {
+export default function Header({ shownav = true }: HeaderProps) {
+  const [user, setUser] = useState<User | null>(null);
+
+  const getUserInfo = async (): Promise<void> => {
+    try {
+      const jwt = sessionStorage.getItem("jwt");
+      const response = await axios.get<User>(`${API_BASE_URL}/users/me`, {
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${jwt}`,
+        },
+      });
+      setUser(response.data);
+    } catch (error: any) {
+      console.error(error);
+    }
+  };
+
+  useEffect(() => {
+    getUserInfo();
+  }, []);
 
   return (
-   <header className="header">
+    <header className="header">
       <div className="header-left">
-        <Link to="/home" className="logo"><img src={logo} alt="MyApp Logo" />
-             Expert services made easy
+        <Link to="/home" className="logo">
+          <img src={logo} alt="MyApp Logo" />
+          Expert services made easy
         </Link>
       </div>
       {shownav && (
-      <nav className="header-right">
-          <Link to="/register-professional" className="nav-link">
-            <FaUserTie className="icon" />  Register as a Professional
-        </Link>
-        <Link to="/my-account" className="nav-link">
-          <FaUserCircle className='icon'/> My Account
-        </Link>
-      </nav>
-      )}
-
-      {!shownav && (
-        <nav className='header-right'>
-          <Link to="/contact-us" className='nav-link'>
-            <FaEnvelope className='icon'/> Contact us
+        <nav className="header-right">
+          {user?.role !== "PROFESSIONAL" && (
+            <Link to="/register-professional" className="nav-link">
+              <FaUserTie className="icon" /> Register as a Professional
+            </Link>
+          )}
+          <Link to="/my-account" className="nav-link">
+            <FaUserCircle className="icon" /> My Account
           </Link>
         </nav>
       )}
-  </header>
-  )
+
+      {!shownav && (
+        <nav className="header-right">
+          <Link to="/contact-us" className="nav-link">
+            <FaEnvelope className="icon" /> Contact us
+          </Link>
+        </nav>
+      )}
+    </header>
+  );
 }
