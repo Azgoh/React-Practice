13a9396e5adb5b066120e574193998ced7d7b3d5
NikolasTsm <ntsamitr@ece.auth.gr>
2025-08-24 16:07:15 +0300

    Move user state to context and add refresh

--- src/App.tsx
+++ src/App.tsx
@@ -8,58 +8,61 @@ import MyAccount from "./component/MyAccount";
 import ProfRegister from "./component/ProfRegister";
 import VerifyEmailPage from "./component/VerifyEmailPage";
 import { Toaster } from "react-hot-toast";
+import { UserProvider } from "./context/UserContext";
 
 function App() {
   const token = sessionStorage.getItem("jwt");
 
   return (
     <>
-      <Toaster
-        position="top-center"
-        toastOptions={{
-          style: {
-            background: "#333",
-            color: "#fff",
-          },
-          success: {
+      <UserProvider>
+        <Toaster
+          position="top-center"
+          toastOptions={{
             style: {
-              background: "#4caf50",
+              background: "#333",
               color: "#fff",
             },
-          },
-          error: {
-            style: {
-              background: "#f44336",
-              color: "#fff",
+            success: {
+              style: {
+                background: "#4caf50",
+                color: "#fff",
+              },
             },
-          },
-        }}
-      />
-      <Routes>
-        <Route
-          path="/"
-          element={
-            token ? (
-              <Navigate to="/home" replace />
-            ) : (
-              <Navigate to="/register" replace />
-            )
-          }
+            error: {
+              style: {
+                background: "#f44336",
+                color: "#fff",
+              },
+            },
+          }}
         />
-        <Route
-          path="/register"
-          element={token ? <Navigate to="/home" replace /> : <SignUpForm />}
-        />
-        <Route
-          path="/login"
-          element={token ? <Navigate to="/home" replace /> : <LoginForm />}
-        />
-        <Route path="/home" element={<HomePage />} />
-        <Route path="/oauth2/redirect" element={<OAuth2RedirectHandler />} />
-        <Route path="/verify-email" element={<VerifyEmailPage />} />
-        <Route path="/my-account" element={<MyAccount />} />
-        <Route path="/register-professional" element={<ProfRegister />} />
-      </Routes>
+        <Routes>
+          <Route
+            path="/"
+            element={
+              token ? (
+                <Navigate to="/home" replace />
+              ) : (
+                <Navigate to="/register" replace />
+              )
+            }
+          />
+          <Route
+            path="/register"
+            element={token ? <Navigate to="/home" replace /> : <SignUpForm />}
+          />
+          <Route
+            path="/login"
+            element={token ? <Navigate to="/home" replace /> : <LoginForm />}
+          />
+          <Route path="/home" element={<HomePage />} />
+          <Route path="/oauth2/redirect" element={<OAuth2RedirectHandler />} />
+          <Route path="/verify-email" element={<VerifyEmailPage />} />
+          <Route path="/my-account" element={<MyAccount />} />
+          <Route path="/register-professional" element={<ProfRegister />} />
+        </Routes>
+      </UserProvider>
     </>
   );
 }
