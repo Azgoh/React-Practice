0ea2df4e07f68a9ecb0fa2d54e15cfb17f0f5e23
NikolasTsm <ntsamitr@ece.auth.gr>
2025-08-24 17:26:39 +0300

    Improve UI

--- src/component/ProfilePage.tsx
+++ src/component/ProfilePage.tsx
@@ -3,6 +3,7 @@ import "./ProfilePage.css";
 import axios from "axios";
 import { API_BASE_URL } from "../config/Config";
 import { type UserWithProfessionalProfile } from "../interface/UserWithProfessionalProfile";
+import { Divider } from "@mui/material";
 
 export const ProfilePage = () => {
   const jwt = sessionStorage.getItem("jwt");
@@ -33,48 +34,74 @@ export const ProfilePage = () => {
   if (isLoading) return <p>Loading profile...</p>;
   if (!myAccount) return <p>No profile information available.</p>;
 
-  return (
-    <div className="user-profile-info">
-      <h2>My Profile</h2>
-      <p>
-        <strong>Name:</strong> {myAccount.userProfile.username}
-      </p>
-      <p>
-        <strong>Email:</strong> {myAccount.userProfile.email}
-      </p>
-      <p>
-        <strong>Role:</strong> {myAccount.userProfile.role}
-      </p>
-      <p>
-        <strong>Auth provider:</strong> {myAccount.userProfile.authProvider}
-      </p>
+  const hasProfessional =
+    myAccount.userProfile.role === "PROFESSIONAL" &&
+    myAccount.professionalProfile;
 
-      {myAccount.userProfile.role === "PROFESSIONAL" &&
-        myAccount.professionalProfile && (
-          <div className="professional-profile-info">
-            <h3>Professional Information</h3>
-            <p>
-              <strong>First Name:</strong>{" "}
-              {myAccount.professionalProfile.firstName}
-            </p>
-            <p>
-              <strong>Last Name:</strong>{" "}
-              {myAccount.professionalProfile.lastName}
-            </p>
-            <p>
-              <strong>Profession: </strong>{" "}
-              {myAccount.professionalProfile.profession}
-            </p>
-            <p>
-              <strong>Phone Number: </strong>{" "}
-              {myAccount.professionalProfile.phone}
-            </p>
-            <p>
-              <strong>Hourly Rate:</strong> $
-              {myAccount.professionalProfile.hourlyRate}
-            </p>
-          </div>
-        )}
+  return (
+    <div
+      className={`profile-container ${
+        hasProfessional ? "with-professional" : "single"
+      }`}
+    >
+      <div className="profile-card">
+        <h2 className="profile-title">My Profile</h2>
+        <span className="details-field">
+          <h4>Username</h4>
+          <span>{myAccount.userProfile.username}</span>
+        </span>
+        <Divider className="profile-fields-divider"></Divider>
+        <span className="details-field">
+          <h4>Email</h4>
+          <span>{myAccount.userProfile.email}</span>
+        </span>
+        <Divider className="profile-fields-divider"></Divider>
+        <span className="details-field">
+          <h4>Role</h4>
+          <span>{myAccount.userProfile.role.toLowerCase()}</span>
+        </span>
+        <Divider className="profile-fields-divider"></Divider>
+        <span className="details-field">
+          <h4>Auth Provider</h4>
+          <span>{myAccount.userProfile.authProvider.toLowerCase()}</span>
+        </span>
+      </div>
+      {hasProfessional && <Divider orientation="vertical" flexItem className="vertical-divider"></Divider>}
+
+      {hasProfessional && (
+        <div className="profile-card">
+          <h2 className="profile-title">My Professional Profile</h2>
+          <span className="details-field">
+            <h4>First Name</h4>
+            <span>{myAccount.professionalProfile?.firstName}</span>
+          </span>
+          <Divider className="profile-fields-divider"></Divider>
+          <span className="details-field">
+            <h4>Last Name</h4>
+            <span>{myAccount.professionalProfile?.lastName}</span>
+          </span>
+          <Divider className="profile-fields-divider"></Divider>
+          <span className="details-field">
+            <h4>Profession</h4>
+            <span>{myAccount.professionalProfile?.profession}</span>
+          </span>
+          <Divider className="profile-fields-divider"></Divider>
+          <span className="details-field">
+            <h4>Location</h4>
+            <span>{myAccount.professionalProfile?.location}</span>
+          </span>
+          <Divider className="profile-fields-divider"></Divider>
+          <span className="details-field">
+            <h4>Phone Number</h4>
+            <span>{myAccount.professionalProfile?.phone}</span>
+          </span>
+          <Divider className="profile-fields-divider"></Divider>
+          <span className="details-field">
+            <h4>Hourly Rate</h4>
+            <span>{myAccount.professionalProfile?.hourlyRate} {"$"}</span>
+          </span>
+        </div>
+      )}
     </div>
   );
 };
