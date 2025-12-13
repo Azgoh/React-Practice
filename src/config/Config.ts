02594691200349d992da84891e1cd2cdc274224d
NikolasTsm <ntsamitr@ece.auth.gr>
2025-12-13 15:50:25 +0200

    Use render api

--- src/config/Config.ts
+++ src/config/Config.ts
@@ -1,2 +1,2 @@
-export const API_BASE_URL = process.env.API_LOCAL_URL;
-export const OAUTH2_BASE_URL = process.env.LOCAL_OAUTH2_BASE_URL;
+export const API_BASE_URL = process.env.RENDER_API_URL;
+export const OAUTH2_BASE_URL = process.env.RENDER_OAUTH2_URL;
