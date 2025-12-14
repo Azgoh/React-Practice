542f87b68cb8617abf9ea167b4d9960ead6d4f84
NikolasTsm <ntsamitr@ece.auth.gr>
2025-12-14 18:40:49 +0200

    Change base url

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,10 +1,13 @@
 import { defineConfig } from "cypress";
 
+const isCI = true; // Set to true for CI environment, false for local development
+
 export default defineConfig({
   e2e: {
-    baseUrl: "https://beasy-13h0.onrender.com",
+    baseUrl: isCI ? "http://localhost:4200" : "https://beasy-13h0.onrender.com",
     setupNodeEvents(on, config) {
       // implement node event listeners here
+      return config;
     },
   },
 });
