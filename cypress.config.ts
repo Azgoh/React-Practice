0139070afed023b8dae0da07430953aabc3ef201
NikolasTsm <ntsamitr@ece.auth.gr>
2025-12-14 18:50:21 +0200

    Tweak url

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,10 +1,12 @@
 import { defineConfig } from "cypress";
 
-const isCI = true; // Set to true for CI environment, false for local development
+const isCI = !!process.env.CI;
 
 export default defineConfig({
   e2e: {
-    baseUrl: isCI ? "http://localhost:4200" : "https://beasy-13h0.onrender.com",
+      baseUrl: isCI
+      ? "http://localhost:4200"           // CI runs local frontend
+      : "https://beasy-13h0.onrender.com",
     setupNodeEvents(on, config) {
       // implement node event listeners here
       return config;
